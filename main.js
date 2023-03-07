72c69ea0e12fe60d5946258fef441c05840d2c98
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-03-08 00:19:44 +0500

    index, css and js commit

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const form = document.querySelector("#form");
 const textInput = document.querySelector("#textInput");
 const list = document.querySelector("#list");
+const modallayer =document.querySelector("#modallayer")
+const EditedInput =document.querySelector("#EditedInput")
+const editForm = document.querySelector("#editForm")
 
-let todoAppText =[];
+
+let todoAppText =[
+    {
+        text:"salom",
+        id: 99999999,
+    }
+];
 let index = 0;
+let editItem ;
+
+renderHtmlElements()
 function createHtmlElementes(e) {
     e.preventDefault();
     todoAppText.push({
@@ -12,16 +24,20 @@ function createHtmlElementes(e) {
     });
      index++
    
+   renderHtmlElements();
+}
+function renderHtmlElements(){
     let result ="";
     todoAppText.forEach((item , index)=>{
         result =result + `<li class="list-item" id="item">${item.text} 
-        <div class="card"><img class="card__img" src="./img/circele-img.png" alt="">
+        <div class="card"><img class="card__img" src="./img/circele-img.png" onclick="showModal(${item.id})" alt="">
         <img src="./img/recucle-img.png" alt="" onclick="removeTodoApp(${item.id})" class="card__img"></div>
         </li>`;
     });
      console.log(result);
      list.innerHTML = result;
 }
+
  
 form.addEventListener("submit", createHtmlElementes);
 
@@ -29,16 +45,49 @@ form.addEventListener("submit", createHtmlElementes);
     function removeTodoApp(id){
        todoAppText = todoAppText.filter((item)=>item.id !==id);
         console.log(todoAppText);
-        let result = "";
 
-        todoAppText.forEach((item, i)=>{
-            result =result + 
-            `<li class="list-item" id="item">${item.text} 
-            <div class="card"><img class="card__img" src="./img/circele-img.png" alt="">
-            <img src="./img/recucle-img.png" alt="" onclick="removeTodoApp(${item.id})" class="card__img"></div>
-            </li>`;
-        });
-        
-         list.innerHTML = result;
+
+        renderHtmlElements();
 
     }
+
+    function showModal(id){
+        modallayer.style.display ="flex";
+
+        let findEndObject = todoAppText.find((item)=>{
+            return item.id === id;
+        });
+        let findEndObjectIndex = todoAppText.findIndex((item)=>{
+            return item.id === id;
+        });
+        EditedInput.value =findEndObject.text ;   
+        
+        editItem ={
+            index: findEndObjectIndex,
+            id: id,
+        }
+    }
+
+    function editFormHandler(e){
+        e.preventDefault();
+
+        todoAppText.splice(editItem.index,1, {
+            text:EditedInput.value,
+            id:editItem.id,
+        });
+
+
+
+        modallayer.style.display ="none";
+
+        renderHtmlElements();
+    }
+
+    editForm.addEventListener("sumbit", editFormHandler)
+
+
+    modallayer.addEventListener('click',(e)=>{
+        if(e.target == modallayer) modallayer.style.display = "none";
+
+    });
+    
